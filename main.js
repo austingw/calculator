99aacb6ea16bd18c1d06f8d7a305cf94b538fac0
Austin Williams <senortinman@gmail.com>
2022-05-05 12:41:58 -0400

    add complete functionality, keyboard input, and basic gui

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ let operator = "";
 const currentDisplayNum = document.querySelector(".currentNum");
 const previousDisplayNum = document.querySelector(".previousNum");
 
+window.addEventListener("keydown", handleKeypress);
+
 const operators = document.querySelectorAll(".operator");
 const numButtons = document.querySelectorAll(".number");
+
 const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", () => {
+    addDecimal();
+});
 
 const equal = document.querySelector(".equal");
-equal.addEventListener("click", calculate);
+equal.addEventListener("click", () => {
+    if (currentNum != "" && previousNum != "") {
+        calculate();
+    }
+});
 
 const clear = document.querySelector(".allClear");
-//clear.addEventListener("click", calculate);
+clear.addEventListener("click", clearCalculator);
 
 numButtons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -22,7 +32,11 @@ numButtons.forEach((btn) => {
 });
 
 function handleNum(number) {
-   if (currentNum.length <= 12) {
+   if (previousNum !== "" && currentNum !== "" && operator === "") {
+       previousNum = ""
+       currentDisplayNum.textContent = currentNum;
+   };
+    if (currentNum.length <= 11) {
     currentNum += number;
    currentDisplayNum.textContent = currentNum;
     };
@@ -35,12 +49,25 @@ operators.forEach((btn) => {
 });
 
 function handleOperators(ops) {
-    operator = ops;
-    previousNum = currentNum;
-    previousDisplayNum.textContent = previousNum + " " + operator;
-    currentNum = "";
-    currentDisplayNum.textContent = "";
+    if (previousNum === "") {
+        previousNum = currentNum;
+        operatorCheck(ops)
+    } else if(currentNum === "") {
+        operatorCheck(ops)
+    } else {
+        calculate();
+        operator = ops;
+        currentDisplayNum.textContent = "0";
+        previousDisplayNum.textContent = previousNum + " " + operator;
+    }
  };
+
+function operatorCheck(text) {
+    operator = text;
+    previousDisplayNum.textContent = previousNum + " " + operator;
+    currentDisplayNum.textContent = "0";
+    currentNum = "";
+}
 
  function calculate() {
     previousNum = Number(previousNum);
@@ -52,8 +79,60 @@ function handleOperators(ops) {
     } else if(operator === "*") {
         previousNum = previousNum * currentNum
     } else if(operator === "÷") {
+        if(currentNum <= 0) {
+            previousNum = "Error";
+            resultDisplay();
+            operator = "";
+            return;
+        }
         previousNum = previousNum / currentNum
     }
-    previousDisplayNum.textContent = "";
-    currentDisplayNum.textContent = previousNum;
+    previousNum = previousNum.toString();
+    resultDisplay();
  };
+
+ function resultDisplay() {
+   
+    if(previousNum.length <= 11) {
+        currentDisplayNum.textContent = previousNum;
+    } else {
+        currentDisplayNum.textContent = previousNum.slice(0,11) + "...";
+    }
+    previousDisplayNum.textContent = "";
+    operator = "";
+    currentNum = "";
+ };
+
+ function clearCalculator() {
+     currentNum = "";
+     previousNum = "";
+     operator = "";
+     currentDisplayNum.textContent = "0";
+     previousDisplayNum.textContent = "";
+     };
+
+ function addDecimal() {
+     if(!currentNum.includes(".")){
+         currentNum += ".";
+         currentDisplayNum.textContent = currentNum;
+     }
+ }
+
+ function handleKeypress(e) {
+     e.preventDefault();
+     if(e.key >= 0 && e.key <= 9) {
+         handleNum(e.key);
+     }
+     if(e.key === "Enter" || e.key === "=" && currentNum != "" && previousNum != "") {
+        calculate();
+     }
+     if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*") {
+         handleOperators(e.key)
+     }
+     if (e.key === ".") {
+         addDecimal(e.key);
+     }
+     if (e.key === "Backspace") {
+         clearCalculator();
+     }
+ }
